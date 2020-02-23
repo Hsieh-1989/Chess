7f8f7524493a3f803e82c4bc7d2e9d05a1e8bef0
Hsieh Min Che <mr.hsieh1989@gmail.com>
2020-02-24 00:34:07 +0800

    chinese chess validation test

---
+++ Tests/XiangqiIVTests/ChessValidationTest.swift
@@ -0,0 +1,167 @@
+//
+//  ChessValidationTest.swift
+//  XiangqiIVTests
+//
+//  Created by Hsieh Min Che on 2020/2/24.
+//
+
+import XCTest
+@testable import XiangqiIV
+
+final class ChessValidationTest: XCTestCase {
+    
+    func testKingValidation() {
+        // test target "將" at (1,1)
+        let data = """
+            ・・・・・
+            ・將士・・
+            ・兵・・・
+            ・・・・・
+            ・・・・・
+        """
+        XCTAssertEqual(
+            try validPath(at: Position(x: 1, y: 1), data: data),
+            [
+                (1, 0),
+                (0, 1),
+                (1, 2)
+            ]
+        )
+    }
+    
+    func testAssistantValidation() {
+        // test target "士" at (1,1)
+        let data = """
+            ・・・・・
+            ・士・・・
+            兵・將・・
+            ・・・・・
+            ・・・・・
+        """
+        XCTAssertEqual(
+            try validPath(at: Position(x: 1, y: 1), data: data),
+            [
+                (0, 0), (2, 0),
+                (0, 2)
+            ]
+        )
+    }
+    
+    func testElephantValidation() {
+        // test target "象" at (2,2)
+        let data = """
+            ・・・・卒
+            ・・・・・
+            ・・象・・
+            ・・・・・
+            兵・・・・
+        """
+        XCTAssertEqual(
+            try validPath(at: Position(x: 2, y: 2), data: data),
+            [
+                (0, 0),
+                (0, 4), (4, 4)
+            ]
+        )
+    }
+    
+    func testHorseValidation() {
+        // test target "馬" at (2,2)
+        let data = """
+            ・・・卒・
+            ・・・・・
+            ・・馬・・
+            ・・・・・
+            ・兵・・・
+        """
+        XCTAssertEqual(
+            try validPath(at: Position(x: 2, y: 2), data: data),
+            [
+                (1, 0),
+                (0, 1), (4, 1),
+                (0, 3), (4, 3),
+                (1, 4), (3, 4)
+            ]
+        )
+    }
+    
+    func testChariotValidation() {
+        // test target "車" at (1,1)
+        let data = """
+            ・兵・・・
+            ・車・帥炮
+            ・・・・・
+            ・兵・・・
+            ・・・・・
+        """
+        XCTAssertEqual(
+            try validPath(at: Position(x: 1, y: 1), data: data),
+            [
+                (1, 0),
+                (0, 1), (2, 1), (3, 1),
+                (1, 2),
+                (1, 3)
+            ]
+        )
+    }
+    
+    func testCannonValidation() {
+        // test target "包" at (0,2)
+        let data = """
+            ・・・・・
+            兵・・・・
+            包・・卒兵
+            ・・・・・
+            ・・・・・
+        """
+        XCTAssertEqual(
+            try validPath(at: Position(x: 0, y: 2), data: data),
+            [
+                (1, 2), (2, 2), (4, 2),
+                (0, 3),
+                (0, 4),
+            ]
+        )
+    }
+    
+    func testSoldierValidation() {
+        // test target "卒" at (1,1)
+        let data = """
+            ・・・・・
+            ・卒士・・
+            ・帥・・・
+            ・・・・・
+            ・・・・・
+        """
+        XCTAssertEqual(
+            try validPath(at: Position(x: 1, y: 1), data: data),
+            [
+                (1, 0),
+                (0, 1),
+                (1, 2)
+            ]
+        )
+    }
+    
+    // MARK: - AllTests
+    static var allTests = [
+        ("testKingValidation", testKingValidation),
+        ("testAssistantValidation", testAssistantValidation),
+        ("testElephantValidation", testElephantValidation),
+        ("testHorseValidation", testHorseValidation),
+        ("testChariotValidation", testChariotValidation),
+        ("testCannonValidation", testCannonValidation),
+        ("testSoldierValidation", testSoldierValidation),
+    ]
+    
+    // MARK: - Private Helper
+    private func validPath(at position: Position, data: String) throws -> [Position] {
+        let board = Board(string: data)
+        guard case let .occupied(chess) = board[position] else {
+            throw "the testing board setup is wrong, current board: \n\(board)"
+        }
+        return chess.validPath(from: position, on: board)
+    }
+}
+
+
---
+++ Tests/XiangqiIVTests/Helper.swift
@@ -0,0 +1,50 @@
+//
+//  Helper.swift
+//  XiangqiIV
+//
+//  Created by Hsieh Min Che on 2020/2/24.
+//
+
+import XCTest
+@testable import XiangqiIV
+
+extension String: Error {}
+
+extension Board {
+    init(string: String) {
+        let array = string
+            .split(separator: "\n")
+            .map { $0.trimmingCharacters(in: .whitespacesAndNewlines) }
+        
+        let width = array.first?.count ?? 0
+        let height = array.count
+        
+        let data = array
+            .joined()
+            .enumerated()
+            .reduce(into: [Position: Chess]()) { result, current in
+                let (index, character) = current
+                guard let chess = ChineseChess(rawValue: String(character)) else { return }
+                let position = Position(x: index % width, y: index / width)
+                result[position] = chess
+        }
+        self = Board(width: width, height: height, data: data)
+    }
+}
+
+public func XCTAssertEqual(
+    _ expression1: @autoclosure () throws -> [Position],
+    _ expression2: @autoclosure () throws -> [(Int, Int)],
+    _ message: @autoclosure () -> String = "",
+    file: StaticString = #file,
+    line: UInt = #line
+) {
+    XCTAssertEqual(
+        try expression1(),
+        try expression2().map(Position.init),
+        message(),
+        file: file,
+        line: line
+    )
+}
+
